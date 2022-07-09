24d7b13c34dfc47d66f44c63b249f63f37187fc7
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-07-09 10:38:14 +0200

    Deploy zone2 and HA zone1

--- project/starter-code/zone2/_config.tf
+++ project/starter-code/zone2/_config.tf
@@ -1,16 +1,16 @@
 terraform {
-   backend "s3" {
-     bucket = "p6-west-1"
-     key    = "terraform/terraform.tfstate"
-     region = "us-west-1"
-   }
- }
+  backend "s3" {
+    bucket = "p6-west-1"
+    key    = "terraform/terraform.tfstate"
+    region = "us-west-1"
+  }
+}
 
- provider "aws" {
-   region = "us-west-1"
-   #profile = "default"
-   
-   default_tags {
-     tags = local.tags
-   }
- }
+provider "aws" {
+  region = "us-west-1"
+  #profile = "default"
+
+  default_tags {
+    tags = local.tags
+  }
+}
---
+++ project/starter-code/zone2/modules/alb/_var.tf
@@ -0,0 +1,5 @@
+variable "name" {}
+variable "subnet_id" {}
+variable "vpc_id" {}
+variable "ec2_sg" {}
+variable "ec2" {}
